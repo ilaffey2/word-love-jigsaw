600ff80293fef19189e9615a5207e83dcc74b5f5
ian <ian.laffey@mail.mcgill.ca>
2023-08-07 10:22:37 -0700

    update hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,16 +5,13 @@ const Hero = () => {
   return (
     <div className="flex flex-col items-center text-center bg-rose-100 rounded-md p-6 my-8">
       <p className="text-xl md:text-2xl text-rose-900 mb-2">
-        Find out how well you and your partner match up in the world of text
-        embeddings!
+        Find out how well you and your partner match up... Mathematically!
       </p>
       <p className="text-md text-rose-800">
         Our love calculator uses a technique called{" "}
-        <strong>textual embeddings</strong> — a form of word association magic
-        that maps words into a high-dimensional space. When two names are
-        inputted, our smart algorithm quantifies how close these names are in
-        this magical space, essentially measuring the{" "}
-        <strong>&quot;similarity&quot;</strong> between them.
+        <strong>textual embeddings</strong> — a form of magic that maps words
+        into a high-dimensional space.<br></br> When two names are inputted, a
+        similarity quantifies how close these names are in this magical space.
       </p>
 
       <div className="flex items-center justify-center mt-4 text-sm text-rose-600">
